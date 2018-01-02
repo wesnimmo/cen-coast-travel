71e8925f6ec0a270f8b22f10148ad4a363cb5f42
Wesley Nimmo <wesnimmo@gmail.com>
2018-01-01 17:46:04 -0800

    Added modernizr

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
-imagemin = require('gulp-imagemin');
+imagemin = require('gulp-imagemin'),
+del = require('del');
 
-gulp.task('optimizeImages', function() {
+gulp.task('deleteDistFolder', function() {
+	return del("./dist");
+});
+
+gulp.task('optimizeImages', ['deleteDistFolder'], function() {
 	return gulp.src('./app/assets/images/**/*')
 		.pipe(imagemin({
 			progressive: true,
@@ -11,5 +16,11 @@ gulp.task('optimizeImages', function() {
 		.pipe(gulp.dest("./dist/assets/images"));
 });
 
+gulp.task('usemin', function() {
+	return gulp.src("./app/index.html")
+	.pipe
+		.pipe(gulp.dest("./dist"));
+});
 
-gulp.task('build', ['optimizeImages']);
+
+gulp.task('build', ['deleteDistFolder', 'optimizeImages']);
